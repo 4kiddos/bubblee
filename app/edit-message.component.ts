49dfd599b25bc277a5dbcbf89e9d9ecbdff6cfb4
4kiddos <sylvain.4kiddos@gmail.com>
2016-09-06 21:09:19 -0500

    working form at the top of the page

--- app/edit-message.component.ts
+++ app/edit-message.component.ts
@@ -3,47 +3,60 @@ import {Message} from './message';
 
 @Component({
     selector: 'edit-message',
-    //     template: `
-    //     <form (ngSubmit)="onSubmit()" #messageForm="ngForm">
-    //         <button type="submit" [disabled]="!messageForm.form.valid">Submit</button>
-    //         <label for="name">Name</label>
-    //         <input type="text" class="form-control" id="name"
-    //             required
-    //             [(ngModel)]="message.name" name="name"
-    //             #name="ngModel" >
-    //         <div [hidden]="name.valid || name.pristine" 
-    //             class="alert alert-danger">
-    //         Name is required
-    //         </div>
-    //     </form>
-    // `,
         template: `
         <form (ngSubmit)="onSubmit()" #messageForm="ngForm">
             <button type="submit" [disabled]="!messageForm.form.valid">Submit</button>
             <div class="message-header-line">
-                <h2>From:</h2>
-                <input type="text" class="form-control" id="from" [(ngModel)]="message.from" name="from" #from="ngModel"/>
+                <label for="from">From</label>
+                <input type="text" class="form-control" id="from"
+                    required
+                    [(ngModel)]="message.from" name="from"
+                    #from="ngModel" >
+                <div [hidden]="from.valid || from.pristine" 
+                    class="alert alert-danger">
+                From is required
+                </div>
             </div>
             <div class="message-header-line">
-                <h2>To:</h2>
-                <input [(ngModel)]="message.to" >
+                <label for="to">To</label>
+                <input type="text" class="form-control" id="to"
+                    required
+                    [(ngModel)]="message.to" name="to"
+                    #to="ngModel" >
+                <div [hidden]="to.valid || to.pristine" 
+                    class="alert alert-danger">
+                To is required
+                </div>
             </div>
             <div class="message-header-line">
-                <h2>Title:</h2>
-                <input [(ngModel)]="message.title" class="title" >
+                <label for="title">Title</label>
+                <input type="text" class="form-control" id="title"
+                    required
+                    [(ngModel)]="message.title" name="title"
+                    #title="ngModel" >
+                <div [hidden]="title.valid || title.pristine" 
+                    class="alert alert-danger">
+                Title is required
+                </div>
             </div>
-            <textarea name="text" style="width:90vw;" >
-                {{message.content}}
-            </textarea>
+            <div class="message-header-line">
+                <textarea [(ngModel)]="message.content" name="content"></textarea>
+            </div>
+        <div class="cheat">{{stringify()}}</div>
         </form>
     `,
+
+                // <textarea class="form-control" id="content"
+                //     [(ngModel)]="message.content" name="content"
+                //     #content="ngModel" >
+
     styles: [`
         .message-header-line{
             display: flex;
             flex-direction: row;
             align-items:    center;
         }
-        h2 {
+        label {
             width: 5vw;
         }
         input {
@@ -52,9 +65,20 @@ import {Message} from './message';
         }
         textarea {
             font-size:  0.8vw;
+            width:      100%;
+            height:     20vh;
         }
         .title {
             width: 80%;
+        }
+        .cheat {
+            color: #888;
+        }
+        form {
+            margin: 2vw;
+        }
+        form div {
+            margin:     0.5vw;
         }
     `] 
 })
@@ -63,4 +87,5 @@ export class EditMessageComponent {
     message :Message = new Message();
     submitted = false;
     onSubmit() { this.submitted = true; }
+    stringify():string {return JSON.stringify(this.message);}
 }
--- app/read-message.component.ts
+++ app/read-message.component.ts
@@ -5,20 +5,18 @@ import {Message} from './message';
     selector: 'read-message',
         template: ` 
         <div class="message-header-line">
-            <h2>From:</h2>
-            <input value="{{message.from}}" [readonly]="message.isReadOnly">
+            <div>From:</div>  
+            <div>{{message.from}}</div>
+            <div>To:  </div>  
+            <div>{{message.to}}  </div>
         </div>
         <div class="message-header-line">
-            <h2>To:</h2>
-            <input value="{{message.to}}" [readonly]="message.isReadOnly">
+            <div>Title:</div> 
+            <div class="title">{{message.title}}</div>
         </div>
-        <div class="message-header-line">
-            <h2>Title:</h2>
-            <input value="{{message.title}}" class="title" [readonly]="message.isReadOnly">
-        </div>
-        <textarea name="text" style="width:90vw;" [readonly]="message.isReadOnly">
+        <div style="width:90vw;">
             {{message.content}}
-        </textarea>
+        </div>
     `,
     styles: [`
         .message-header-line{
@@ -26,18 +24,16 @@ import {Message} from './message';
             flex-direction: row;
             align-items:    center;
         }
-        h2 {
+        div div {
             width: 5vw;
         }
-        input {
-            height:     1.5vw
+        div {
+            height:     2vw;
             font-size:  0.8vw;
-        }
-        textarea {
-            font-size:  0.8vw;
+            margin-right: 1vw;
         }
         .title {
-            width: 80%;
+            width: 50vw;
         }
     `] 
 })
